1171bfcd89d1656746491cbdf9af89b0fbe92443
Love to code my life <47113916+vaishnavan@users.noreply.github.com>
2022-10-19 20:55:12 +0530

    small change

--- controller/transport.controller.js
+++ controller/transport.controller.js
@@ -15,38 +15,35 @@ const addTransportBill = (req, res) => {
 }
 
 const getTransportBill = (req, res) => {
-  Transport.find().then((data) => {
-    res.json(data)
-  })
-  // try {
-  //   const getBillData = Transport.find()
-  //   return res.status(200).json({
-  //     message: 'Data fetched successfully',
-  //     result: getBillData,
-  //   })
-  // } catch (error) {
-  //   return res.status(400).json({ message: error.message })
-  // }
+  Transport.find()
+    .then((data) => {
+      res.json(data)
+    })
+    .catch((err) => {
+      res.json({ message: err.message })
+    })
 }
 
 const updateTransportBill = (req, res) => {
-  try {
-    const billData = req.body
-    const transport = new Transport({ _id: req.params.id, ...billData })
-    Transport.updateOne({ _id: req.params.id }, transport)
-    return res.status(200).json({ message: 'Updated Successfully' })
-  } catch (error) {
-    return res.status(400).json({ message: error.message })
-  }
+  const billData = req.body
+  const transport = new Transport({ _id: req.params.id, ...billData })
+  Transport.updateOne({ _id: req.params.id }, transport)
+    .then(() => {
+      return res.status(200).json({ message: 'Updated successfully' })
+    })
+    .catch((err) => {
+      return res.status(400).json({ message: err.message })
+    })
 }
 
 const deleteTransportBill = (req, res) => {
-  try {
-    Transport.deleteOne({ _id: req.params.id })
-    return res.status(200).json({ message: 'Removed Successfully' })
-  } catch (error) {
-    return res.status(400).json({ message: error.message })
-  }
+  Transport.deleteOne({ _id: req.params.id })
+    .then(() => {
+      return res.status(200).json({ message: 'Removed Successfully' })
+    })
+    .catch((err) => {
+      return res.status(400).json({ message: err.message })
+    })
 }
 
 module.exports = {
